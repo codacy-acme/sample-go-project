d05cda03be269c6bb38e41392ad6c635b5c1bcbd
Ranga Ngwerume <Ranga.ngwerume@codacy.com>
2024-01-10 09:46:28 +0000

    Added tests

--- hello/main.go
+++ hello/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"testing"
 	"time"
 )
 
@@ -11,7 +12,7 @@ func main() {
 	unusedVariable := "This variable is not used"
 
 	// Common error: Unused import
-	 import "fmt"
+	// import "fmt"
 
 	// Common error: Printf format string without arguments
 	// fmt.Printf("Hello, World!")
@@ -32,20 +33,47 @@ func main() {
 	// }
 
 	// Uncomment the following line to introduce a runtime error
-	 fmt.Println(undefinedVariable)
+	// fmt.Println(undefinedVariable)
 
 	// Print "Hello, World!" with a random continent
 	fmt.Printf("Hello, World from %s!\n", getRandomContinent())
 }
 
 // Common error: Missing return type in function declaration
- func add(x, y) {
-     return x + y
- }
+func add(x, y int) int {
+	return x + y
+}
 
 // getRandomContinent returns a random continent name
 func getRandomContinent() string {
 	continents := []string{"Africa", "Antarctica", "Asia", "Europe", "North America", "Oceania", "South America"}
 	rand.Seed(time.Now().UnixNano())
 	return continents[rand.Intn(len(continents))]
+}
+
+// Tests
+
+func TestAdd(t *testing.T) {
+	result := add(3, 5)
+	expected := 8
+	if result != expected {
+		t.Errorf("Expected %d, but got %d", expected, result)
+	}
+}
+
+func TestGetRandomContinent(t *testing.T) {
+	continents := []string{"Africa", "Antarctica", "Asia", "Europe", "North America", "Oceania", "South America"}
+	result := getRandomContinent()
+
+	found := false
+	for _, continent := range continents {
+		if continent == result {
+			found = true
+			break
+		}
+	}
+
+	if !found {
+		t.Errorf("Unexpected continent: %s", result)
+	}
 }
